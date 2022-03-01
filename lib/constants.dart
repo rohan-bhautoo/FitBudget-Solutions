82066339e5fff9c997f22f0f469a9e206d200f2a
Rohan Bhautoo <rohanbhautoo@gmail.com>
2022-03-01 21:03:51 +0400

    Added assets and constants

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFe99aff);
+const kTextColor = Color(0xFF757575);
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
+import 'routes.dart';
 import 'package:flutter/material.dart';
 
+import 'size_config.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const FinanceApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class FinanceApp extends StatelessWidget {
+  const FinanceApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      supportedLocales: const [Locale('en', 'US')],
+      title: 'Finance App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      routes: routes,
+      home: const LandingPage(),
     );
+  }
+}
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Column();
   }
 }
